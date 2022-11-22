1368cf4f50670f8fb7fb3f381432d9c61eaf94d2
Savio Picanço <saviopbrito@outlook.com>
2022-11-22 16:46:14 -0300

    feat: add create user service

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,7 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 import { UserDto } from './protocols/user-dto';
 
 @Injectable()
 export class UserService {
-  async create(data: UserDto) {}
+  private readonly prisma: PrismaClient;
+  constructor(prisma: PrismaClient) {
+    this.prisma = prisma;
+  }
+  async create(data: UserDto) {
+    const userAlreadyExists = await this.prisma.user.findFirst({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (userAlreadyExists) {
+      throw new Error('User Already Exists');
+    }
+
+    const user = await this.prisma.user.create({
+      data,
+    });
+
+    return user;
+  }
 }
