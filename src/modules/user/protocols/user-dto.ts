3aabfa947dcfdc1ba2e52471b98ae538a4a0a65d
Savio Picanço <saviopbrito@outlook.com>
2022-11-23 16:25:28 -0300

    refactor: add class-validators to user

--- src/modules/user/protocols/user-dto.ts
+++ src/modules/user/protocols/user-dto.ts
@@ -1,11 +1,25 @@
 import { User } from '../entity/user';
-
+import {
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 export class UserDto extends User {
   id?: string;
 
+  @IsString()
   name: string;
 
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
   password: string;
 
+  @IsEmail()
   email: string;
 }
