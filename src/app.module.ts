eafb83dd6cc14fd26f884dd6b85f5ff292539567
Savio Picanço <saviopbrito@outlook.com>
2022-11-23 23:57:25 -0300

    feat: add guard to auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './infra/auth/auth.module';
+import { AuthService } from './infra/auth/auth.service';
 
 @Module({
   imports: [UserModule, AuthModule],
   controllers: [],
-  providers: [],
+  providers: [AuthService],
 })
 export class AppModule {}
--- src/infra/auth/auth.controller.ts
+++ src/infra/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
-@Controller('auth')
-export class AuthController {}
+@Controller()
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  login() {
+    //return this.authService.login();
+  }
+}
