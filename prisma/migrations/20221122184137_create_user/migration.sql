a33c735283821a3e40680cdc2479d6d8c39e6691
Savio Picanço <saviopbrito@outlook.com>
2022-11-28 17:00:00 -0300

    feat: create docker setup to app and database

--- prisma/migrations/20221122184137_create_user/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "books" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "books_email_key" ON "books"("email");
